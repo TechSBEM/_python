abfc28e7978ac182bb821e6bd56b7c9a2b59b318
Emmanuel <sarpbe808@gmail.com>
2025-03-11 10:32:44 -1200

    while loop

---
+++ python_statements/while_loop.py
@@ -0,0 +1,19 @@
+x =0
+
+while x <5:
+    print(f"my value of x is {x}")
+    x = x+1
+
+
+"""
+break: Break out of the current closest enclosing loop
+continur: Goes to the top of the closest enclosing loop
+pass: Does nothing at all
+"""
+
+x= [1,2,3]
+
+# Suppose I don't want to do anything for now
+for num in x:
+    # Just do nothing
+    pass
