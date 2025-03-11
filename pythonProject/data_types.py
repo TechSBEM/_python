c6d81e0dd4de5eb8b23c0dac049518943c497e76
Emmanuel <sarpbe808@gmail.com>
2025-03-11 06:07:10 -1200

    Data_structures

---
+++ pythonProject/data_types.py
@@ -0,0 +1,15 @@
+a= 10
+
+print(type(a))
+
+# Slicing
+# String[start: stop: step]
+
+name= 'Emmanuel'
+# Start from the '2' index
+print(name[2::])
+
+# printing from index '0' to the last with a step of 2
+print(name[::2])
+# Reversing it
+print(name[::-1])
